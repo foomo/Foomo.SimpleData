df20a1442e711255280be359af1cadb9d6a0f4a3
Jan Halfar <jan@bestbytes.com>
2013-02-27 12:17:51 +0100

    removed unused variable

--- lib/Foomo/SimpleData.php
+++ lib/Foomo/SimpleData.php
@@ -5,7 +5,6 @@ namespace Foomo;
 class SimpleData {
 	public static function read($rootFolder, array $validators = array())
 	{
-		$ret = array();
 		$crawler = new SimpleData\Crawler($rootFolder, $validators);
 		$crawler->crawl();
 		$result = new SimpleData\CrawlerResult;
